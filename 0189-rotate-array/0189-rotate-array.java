1d5e3cd2cce79de7318b8a7ee9788de48967f30d
Owen <heewon.dev@gmail.com>
2024-01-31 22:22:18 +0900

    Time: 2 ms (32.89%), Space: 57.5 MB (31.49%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,7 +1,8 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int movement = k % nums.length;
-        int[] copied = nums.clone();
+        int[] copied = new int[nums.length];
+        System.arraycopy(nums, 0, copied, 0, nums.length);
 
         for(int index = 0; index < nums.length; index++) {
             int shiftedIndex = index - movement < 0 ? nums.length + (index - movement) : index - movement;
